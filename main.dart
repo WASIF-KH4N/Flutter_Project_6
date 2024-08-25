4e4c364c733a9a51b0481b3d5d7ae0310dd7cce4
Wasif Khan <m.wasif9905@gmail.com>
2024-08-25 17:00:39 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -13,12 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home:MyHomePage()
     );
   }
